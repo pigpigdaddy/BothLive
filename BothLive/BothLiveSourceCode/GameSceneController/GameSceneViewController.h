5414a4cd859121316d1bc514f196d73becf2cd2b
linfengchina <linfeng@linfeng-iOS.local>
2014-10-27 09:30:19 +0800

    update structure
    update structure

---
+++ BothLive/BothLiveSourceCode/GameSceneController/GameSceneViewController.h
@@ -0,0 +1,33 @@
+//
+//  GameSceneViewController.h
+//  BothLive
+//
+//  Created by 林峰 on 14-10-23.
+//  Copyright (c) 2014年 林峰. All rights reserved.
+//
+
+/*!
+ *  @Author pigpigdaddy, 14-10-23 11:10:07
+ *
+ *  @brief  游戏场景控制器
+ */
+
+#import <UIKit/UIKit.h>
+
+@protocol GameSceneViewControllerDelegate <NSObject>
+
+/*!
+ *  @Author pigpigdaddy, 14-10-23 11:10:42
+ *
+ *  @brief  撞击了
+ *
+ *  @param position 撞击的跑酷小孩位置 从上到下index值 从1开始
+ *  @param time     总持续时间
+ */
+- (void)runningCrash:(int)position totalTime:(NSString *)time;
+
+@end
+
+@interface GameSceneViewController : UIViewController
+
+@end
---
+++ BothLive/BothLiveSourceCode/GameSceneController/GameSceneViewController.m
@@ -0,0 +1,28 @@
+//
+//  GameSceneViewController.m
+//  BothLive
+//
+//  Created by 林峰 on 14-10-23.
+//  Copyright (c) 2014年 林峰. All rights reserved.
+//
+
+#import "GameSceneViewController.h"
+
+@interface GameSceneViewController ()
+
+@end
+
+@implementation GameSceneViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
