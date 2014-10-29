9bdd957a718ee952b366ff9f742afe976290e35b
pigpigdaddy <pigpigdaddy@gmail.com>
2014-10-29 21:52:18 +0800

    update menu animationView
    update menu animationView

---
+++ BothLive/BothLiveSourceCode/MenuViewController/MenuView/MenuAnimationView/MenuAnimationView.h
@@ -0,0 +1,13 @@
+//
+//  MenuAnimationView.h
+//  BothLive
+//
+//  Created by linfeng on 14-10-29.
+//  Copyright (c) 2014年 林峰. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MenuAnimationView : UIView
+
+@end
---
+++ BothLive/BothLiveSourceCode/MenuViewController/MenuView/MenuAnimationView/MenuAnimationView.m
@@ -0,0 +1,22 @@
+//
+//  MenuAnimationView.m
+//  BothLive
+//
+//  Created by linfeng on 14-10-29.
+//  Copyright (c) 2014年 林峰. All rights reserved.
+//
+
+#import "MenuAnimationView.h"
+
+@implementation MenuAnimationView
+
+- (instancetype)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        //
+    }
+    return self;
+}
+
+@end
