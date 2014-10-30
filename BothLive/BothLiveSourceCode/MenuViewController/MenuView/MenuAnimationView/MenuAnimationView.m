411f3fbff5b3d786bb7c21b3782ff90d86bd12ff
linfengchina <linfeng@linfeng-iOS.local>
2014-10-30 15:46:20 +0800

    update menu view Animation
    update menu view Animation

--- BothLive/BothLiveSourceCode/MenuViewController/MenuView/MenuAnimationView/MenuAnimationView.m
+++ BothLive/BothLiveSourceCode/MenuViewController/MenuView/MenuAnimationView/MenuAnimationView.m
@@ -8,6 +8,13 @@
 
 #import "MenuAnimationView.h"
 
+#define LIGHT_ANI_KEY            @"LightAnimation_Key"
+#define BIG_CLOUD_ANI_KEY        @"BigCloudAnimation_Key"
+#define SMALL_CLOUD_ANI_KEY      @"SmallCloudAnimation_Key"
+#define UFO_ANI_KEY              @"UfoAnimationKey"
+#define NAME_BIG_ANI_KEY         @"BigNameAnimationKey"
+
+
 @interface MenuAnimationView()
 
 @property (nonatomic, strong)UIImageView *imageViewLight;
@@ -18,6 +25,7 @@
 @property (nonatomic, strong)UIImageView *imageViewEngName;
 @property (nonatomic, strong)UIImageView *imageViewChiNameBig;
 @property (nonatomic, strong)UIImageView *imageViewChiNameSmall;
+@property (nonatomic, strong)UIImageView *imageViewReflect;
 
 @end
 
@@ -33,11 +41,111 @@
     return self;
 }
 
+/*!
+ *  @Author pigpigdaddy, 14-10-30 13:10:05
+ *
+ *  @brief  初始化界面
+ */
 - (void)initView
 {
     [self initBGView];
+    [self initClouds];
+    [self initUFO];
+    [self initBoy];
+    [self initReflact];
+    [self initName];
 }
 
+/*!
+ *  @Author pigpigdaddy, 14-10-30 13:10:14
+ *
+ *  @brief  初始化倒影
+ */
+- (void)initReflact
+{
+    UIImage *image = [UIImage imageNamed:@"menu_animationView_reflact.png"];
+    self.imageViewReflect = [[UIImageView alloc] initWithImage:image];
+    self.imageViewReflect.frame = CGRectMake(self.center.x-160, self.center.y+25, image.size.width+4, image.size.height);
+    [self addSubview:self.imageViewReflect];
+}
+
+/*!
+ *  @Author pigpigdaddy, 14-10-30 13:10:13
+ *
+ *  @brief  初始化app名
+ */
+- (void)initName
+{
+    UIImage *imagePao = [UIImage imageNamed:@"menu_animationView_letter_pao.png"];
+    self.imageViewChiNameBig = [[UIImageView alloc] initWithImage:imagePao];
+    self.imageViewChiNameBig.frame = CGRectMake(self.center.x-130, self.center.y-49, imagePao.size.width, imagePao.size.height);
+    [self addSubview:self.imageViewChiNameBig];
+    
+    UIImage *imageSmallName = [UIImage imageNamed:@"menu_animationView_letter_khjy.png"];
+    self.imageViewChiNameSmall = [[UIImageView alloc] initWithImage:imageSmallName];
+    self.imageViewChiNameSmall.frame = CGRectMake(self.center.x-43, self.center.y-9, imageSmallName.size.width, imageSmallName.size.height);
+    [self addSubview:self.imageViewChiNameSmall];
+    
+    UIImage *imageEngName = [UIImage imageNamed:@"menu_animationView_letter_eng.png"];
+    self.imageViewEngName = [[UIImageView alloc] initWithImage:imageEngName];
+    self.imageViewEngName.frame = CGRectMake(self.center.x+35, self.center.y-29, imageEngName.size.width, imageEngName.size.height);
+    [self addSubview:self.imageViewEngName];
+}
+
+/*!
+ *  @Author pigpigdaddy, 14-10-30 13:10:23
+ *
+ *  @brief  初始化小人
+ */
+- (void)initBoy
+{
+    UIImage *imageBoy = [UIImage imageNamed:@"run1.png"];
+    self.imageViewBoy = [[UIImageView alloc] initWithFrame:CGRectMake(self.center.x-15, self.center.y-63, imageBoy.size.width+15, imageBoy.size.height+24)];
+    self.imageViewBoy.image = imageBoy;
+    NSMutableArray *images = [NSMutableArray array];
+    for (int i = 1; i<=20; i++) {
+        [images addObject:[UIImage imageNamed:[NSString stringWithFormat:@"run%d.png", i]]];
+    }
+    self.imageViewBoy.animationImages = images;
+    [self addSubview:self.imageViewBoy];
+}
+
+/*!
+ *  @Author pigpigdaddy, 14-10-30 13:10:29
+ *
+ *  @brief  初始化UFO
+ */
+- (void)initUFO
+{
+    UIImage *imageBigUFO = [UIImage imageNamed:@"menu_animationView_ufo.png"];
+    self.imageViewUfo = [[UIImageView alloc] initWithImage:imageBigUFO];
+    self.imageViewUfo.frame = CGRectMake(self.center.x-85, self.center.y-115, imageBigUFO.size.width, imageBigUFO.size.height);
+    [self addSubview:self.imageViewUfo];
+}
+
+/*!
+ *  @Author pigpigdaddy, 14-10-30 13:10:36
+ *
+ *  @brief  初始化云
+ */
+- (void)initClouds
+{
+    UIImage *imageBigCloud = [UIImage imageNamed:@"menu_animationView_cloud_large.png"];
+    self.imageViewBigCloud = [[UIImageView alloc] initWithImage:imageBigCloud];
+    self.imageViewBigCloud.frame = CGRectMake(-imageBigCloud.size.width, 110, imageBigCloud.size.width, imageBigCloud.size.height);
+    [self addSubview:self.imageViewBigCloud];
+    
+    UIImage *imageSmallCloud = [UIImage imageNamed:@"menu_animationView_cloud_small.png"];
+    self.imageViewSmallCloud = [[UIImageView alloc] initWithImage:imageSmallCloud];
+    self.imageViewSmallCloud.frame = CGRectMake(self.bounds.size.width+imageSmallCloud.size.width, 160, imageSmallCloud.size.width, imageSmallCloud.size.height);
+    [self addSubview:self.imageViewSmallCloud];
+}
+
+/*!
+ *  @Author pigpigdaddy, 14-10-30 13:10:45
+ *
+ *  @brief  初始化背景图
+ */
 - (void)initBGView
 {
     UIImage *imageSky = [UIImage imageNamed:@"menu_animationView_sky.png"];
@@ -64,7 +172,98 @@
  */
 - (void)startAnimation
 {
-    
+    [self.imageViewBoy startAnimating];
+    [self startCloudAnimation:YES];
+    [self startLightAnimation:YES];
+    [self startUFOAnimation:YES];
+}
+
+/*!
+ *  @Author pigpigdaddy, 14-10-30 13:10:23
+ *
+ *  @brief  开始或关闭 射线的动画
+ *
+ *  @param start 是否开始
+ */
+- (void)startLightAnimation:(BOOL)start
+{
+    if (start) {
+        CABasicAnimation *animation = [CABasicAnimation animationWithKeyPath:@"transform.rotation.z"];
+        animation.toValue=[NSNumber numberWithFloat:-M_PI*2];
+        
+        // 设置其他动画属性
+        animation.duration=6.0;
+        animation.repeatCount=HUGE_VALF;
+        
+        [self.imageViewLight.layer addAnimation:animation forKey:LIGHT_ANI_KEY];
+    }else{
+        [self.imageViewLight.layer removeAllAnimations];
+    }
+}
+
+/*!
+ *  @Author pigpigdaddy, 14-10-30 13:10:17
+ *
+ *  @brief  开始或关闭 云朵的动画
+ *
+ *  @param start 是否开始
+ */
+- (void)startCloudAnimation:(BOOL)start
+{
+    if (start) {
+        // 大云
+        CABasicAnimation *animationBig = [CABasicAnimation animationWithKeyPath:@"position"];
+        animationBig.toValue=[NSValue valueWithCGPoint:CGPointMake(self.imageViewBigCloud.layer.position.x+self.bounds.size.width+self.imageViewBigCloud.frame.size.width, self.imageViewBigCloud.layer.position.y)];
+        
+        // 设置其他动画属性
+        animationBig.duration=10.0;
+        animationBig.repeatCount=HUGE_VALF;
+        
+        [self.imageViewBigCloud.layer addAnimation:animationBig forKey:BIG_CLOUD_ANI_KEY];
+        
+        // 小云
+        CABasicAnimation *animationSmall = [CABasicAnimation animationWithKeyPath:@"position"];
+        animationSmall.toValue=[NSValue valueWithCGPoint:CGPointMake(0-self.imageViewSmallCloud.frame.size.width, self.imageViewSmallCloud.layer.position.y)];
+        
+        // 设置其他动画属性
+        animationSmall.duration=5.0;
+        animationSmall.repeatCount=HUGE_VALF;
+        
+        [self.imageViewSmallCloud.layer addAnimation:animationSmall forKey:SMALL_CLOUD_ANI_KEY];
+    }else{
+        [self.imageViewBigCloud.layer removeAllAnimations];
+        [self.imageViewSmallCloud.layer removeAllAnimations];
+    }
+}
+
+/*!
+ *  @Author pigpigdaddy, 14-10-30 13:10:04
+ *
+ *  @brief  开始或关闭 UFO的动画
+ *
+ *  @param start 是否开始
+ */
+- (void)startUFOAnimation:(BOOL)start
+{
+    if (start) {
+        CAKeyframeAnimation *keyframeAnimation=[CAKeyframeAnimation animationWithKeyPath:@"position"];
+        
+        // 四个关键帧
+        NSValue *key1=[NSValue valueWithCGPoint:self.imageViewUfo.layer.position];// 对于关键帧动画初始值不能省略
+        NSValue *key2=[NSValue valueWithCGPoint:CGPointMake(self.imageViewUfo.layer.position.x, self.imageViewUfo.layer.position.y-12)];
+        NSValue *key3=[NSValue valueWithCGPoint:CGPointMake(self.imageViewUfo.layer.position.x, self.imageViewUfo.layer.position.y+12)];
+        NSValue *key4=[NSValue valueWithCGPoint:self.imageViewUfo.layer.position];
+        NSArray *values=@[key1,key2,key3,key4];
+        keyframeAnimation.values=values;
+        // 设置其他属性
+        keyframeAnimation.duration=2.0;
+        keyframeAnimation.repeatCount=HUGE_VALF;
+        
+        // 添加动画到图层，添加动画后就会执行动画
+        [self.imageViewUfo.layer addAnimation:keyframeAnimation forKey:UFO_ANI_KEY];
+    }else{
+        [self.imageViewUfo.layer removeAllAnimations];
+    }
 }
 
 /*!
@@ -74,7 +273,10 @@
  */
 - (void)stopAnimation
 {
-    
+    [self.imageViewBoy stopAnimating];
+    [self startCloudAnimation:NO];
+    [self startLightAnimation:NO];
+    [self startUFOAnimation:NO];
 }
 
 @end
--- BothLive/BothLiveSourceCode/MenuViewController/MenuViewController.m
+++ BothLive/BothLiveSourceCode/MenuViewController/MenuViewController.m
@@ -38,11 +38,26 @@
 {
     self.menuView = [[MenuView alloc] initWithFrame:self.view.bounds withDelegate:self menuData:self.listDataArray];
     [self.view addSubview:self.menuView];
+    [self.menuView.menuAnimationView startAnimation];
 }
 
 - (void)initMenuListData
 {
     self.listDataArray = [NSArray arrayWithObjects:[NSDictionary dictionaryWithObjectsAndKeys:[NSArray arrayWithObjects:[NSString stringWithFormat:@"%d", MENU_LIST_BUTTON_NAME_GAME_NORMAL], nil], [NSString stringWithFormat:@"%d", MENU_LIST_BUTTON_NAME_START], nil], [NSDictionary dictionaryWithObjectsAndKeys:[NSArray array], [NSString stringWithFormat:@"%d", MENU_LIST_BUTTON_NAME_RANK_LIST], nil], nil];
+}
+
+#pragma mark
+#pragma mark ============ MenuViewDelegate ============
+/*!
+ *  @Author pigpigdaddy, 14-10-27 15:10:06
+ *
+ *  @brief  开始游戏
+ *
+ *  @param gameType 游戏类型
+ */
+- (void)beginGame:(MENU_LIST_BUTTON_NAME)gameType
+{
+    
 }
 
 @end
